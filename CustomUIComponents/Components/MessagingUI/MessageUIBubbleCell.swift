677b4f11240c26f3c1e6aa19d912a1a47b0a9406
Fabio <bwtdirect@gmail.com>
2020-05-03 03:22:01 -0700

    [Fabio] - Fixng merge

---
+++ CustomUIComponents/Components/MessagingUI/MessageUIBubbleCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageUIBubbleCell.swift
+//  CustomUIComponents
+//
+//  Created by Fabio Quintanilha on 5/3/20.
+//  Copyright © 2020 TaskHackers. All rights reserved.
+//
+
+import UIKit
+
+class MessageUIBubbleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
